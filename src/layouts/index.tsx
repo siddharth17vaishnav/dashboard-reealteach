af9c0afe01530cb0a567dff935b9857d9ec0d415
Siddharth <siddharth17vaishnav@gmail.com>
2023-11-05 13:43:44 +0530

    added auth layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from 'react'
 import Drawer from './Drawer'
 import { Box } from '@mui/material'
+import { usePathname } from 'next/navigation'
 
 interface Props {
     children: ReactNode
 }
 
+const AuthLayout = ({ children }: Props) => <>{children}</>
+
 const Layout = ({ children }: Props) => {
+    const pathName = usePathname()
+    if (pathName.includes('auth')) return <AuthLayout>{children}</AuthLayout>
     return (
         <div>
             <Drawer >
